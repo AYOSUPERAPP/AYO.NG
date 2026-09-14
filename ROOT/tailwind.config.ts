5ab3abf3cfd85bae88411fecd6e1f7f70c342f02
AYOSUPERAPP <blackyokiki@gmail.com>
2026-09-14 07:22:01 +0100

    Delete ROOT directory

--- ROOT/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'tailwindcss'
-const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}','./components/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: { colors: { ayo: { green: '#008751' } } } },
-  plugins: [],
-}
-export default config
